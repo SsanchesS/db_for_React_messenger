79f88bdef05ad5d2deb7313d487c0710faea64cd
Alexander <100308427+SsanchesS@users.noreply.github.com>
2024-02-09 11:48:55 +0300

    Delete src/resolvers/check_login.py

--- src/resolvers/check_login.py
+++
@@ -1,6 +0,0 @@
-from sql_base.models import LoginM 
-from sql_base.base import base_worker
-
-def check_login_request(user: LoginM):
-    user_data_list = base_worker.insert_data(f"SELECT * FROM users WHERE email = ? AND password = ?",(user.email,user.password))
-    return user_data_list
